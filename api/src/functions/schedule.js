19a3505781b9325cf73b3b3fa77e22e73e0588a8
Jussi Lehtinen <jussi.lehtinen@pp.inet.fi>
2024-01-30 00:29:17 +0200

    ice rink schedule data

--- api/src/functions/schedule.js
+++ api/src/functions/schedule.js
@@ -1,5 +1,18 @@
 const { app } = require('@azure/functions');
 const fetch = require("node-fetch");
+var moment = require('moment'); // require
+
+const getMonday = () => {
+    const dt = new Date();
+    const day = dt.getDay()
+    const diff = dt.getDate() - day + (day === 0 ? -6 : 1);
+    return new Date(dt.setDate(diff));
+}
+
+function addWeeks(date, weeks) {
+    date.setDate(date.getDate() + 7 * weeks);
+    return date;
+}
 
 app.http('schedule', {
     methods: ['GET', 'POST'],
@@ -7,9 +20,19 @@ app.http('schedule', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const server = 'https://valkeakoski.tilamisu.fi'
-        const requestUri = '/fi/locations/836/reservations.json?timeshift=-120&from=2024-01-15&to=2024-01-22'
+        const now = new Date()
+        const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay() + 1)
+        const endOfWeek = new Date(now.getFullYear(), now.getMonth(), startOfWeek.getDate() + 7)
+        console.log(startOfWeek)
 
+
+        var formattedStart = moment(startOfWeek).format('YYYY-MM-DD')
+        var formattedEnd = moment(endOfWeek).format('YYYY-MM-DD')
+
+        const server = 'https://valkeakoski.tilamisu.fi'
+        const requestUri = '/fi/locations/836/reservations.json?timeshift=-120&from=' + formattedStart + '&to=' + formattedEnd
+
+        console.log(server + requestUri)
         const response = fetch(server + requestUri)
 
         //const name = request.query.get('name') || await request.text() || 'world';
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,10 +1,16 @@
 // Filename - pages/schedule.js
 
-import React from "react";
+import React, { Fragment } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import bootstrapPlugin from '@fullcalendar/bootstrap';
 
-
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css'; // needs additional webpack config!
+import './fullcalendar.css'
 
 class Schedule extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -12,7 +18,7 @@ class Schedule extends React.Component {
         }
     }
 
-    componentDidMount() {
+    componentDidMount() {   
         fetch('api/schedule')
             .then(response => response.json())
             .then(data => {
@@ -24,24 +30,55 @@ class Schedule extends React.Component {
 
     render() {
         const { items } = this.state
+        var events = [];
+        items.map(item => (
+            events.push({
+                id: item.id,
+                title: item.text,
+                start: item.start_date,
+                end: item.end_date
+            })
+        ))
 
-        //items.map(item => {
-        //    console.log(item)
-        //})
         return (
+            <Fragment>
             <div>
-            {
-                items.map(item => (
-                <div key={item.id}>
-                    <h5>{item.text}</h5>
-                    <div>
-                    <p>{item.start_date} - {item.end_date}</p>
-                    </div>
-                </div>
-            ))
-            }
+                <FullCalendar
+                        plugins={[ bootstrapPlugin, timeGridPlugin ]}
+                        initialView="timeGridWeek"
+                        weekends={true}
+                        allDaySlot={false}
+                        eventMinHeight={30}
+                        slotDuration="00:30:00"
+                        slotMinTime="08:00:00"
+                        slotMaxTime="23:30:00"
+                        headerToolbar={{
+                            left: '',
+                            center: 'title',
+                            right: '' // user can switch between the two
+                        }}
+                        slotLabelFormat={{
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            omitZeroMinute: false,
+                            meridiem: 'short',
+                            hour12: false
+                        }}
+                        firstDay={1}
+                        eventColor='orange'
+                        eventBorderColor='orange'
+                        eventTextColor='black'
+                        themeSystem='bootstrap'
+                        now={null}
 
+                        events = {events}
+                        //events={[
+                        //    { title: 'Kiekko-Ahma U12', start: new Date(2024, 0, 29, 14, 0, 0), end: new Date(2024, 0, 29, 14, 50, 0) },
+                        //    { title: 'Kiekko-Ahma U13',start: new Date(2024, 0, 29, 15, 0, 0), end: new Date(2024, 0, 29, 15, 50, 0) }
+                        //]}
+                    />
             </div>
+            </Fragment>
         );
     }
 }
