e38c1badd6f543365dc6f580b2afbe3f196d3710
Jussi Lehtinen <jussi.lehtinen@pp.inet.fi>
2024-01-30 07:11:15 +0200

    clean-up

--- api/src/functions/schedule.js
+++ api/src/functions/schedule.js
@@ -1,18 +1,6 @@
 const { app } = require('@azure/functions');
 const fetch = require("node-fetch");
-var moment = require('moment'); // require
-
-const getMonday = () => {
-    const dt = new Date();
-    const day = dt.getDay()
-    const diff = dt.getDate() - day + (day === 0 ? -6 : 1);
-    return new Date(dt.setDate(diff));
-}
-
-function addWeeks(date, weeks) {
-    date.setDate(date.getDate() + 7 * weeks);
-    return date;
-}
+var moment = require('moment');
 
 app.http('schedule', {
     methods: ['GET', 'POST'],
@@ -23,8 +11,6 @@ app.http('schedule', {
         const now = new Date()
         const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay() + 1)
         const endOfWeek = new Date(now.getFullYear(), now.getMonth(), startOfWeek.getDate() + 7)
-        console.log(startOfWeek)
-
 
         var formattedStart = moment(startOfWeek).format('YYYY-MM-DD')
         var formattedEnd = moment(endOfWeek).format('YYYY-MM-DD')
@@ -34,8 +20,6 @@ app.http('schedule', {
 
         console.log(server + requestUri)
         const response = fetch(server + requestUri)
-
-        //const name = request.query.get('name') || await request.text() || 'world';
 
         return { body: (await response).body };
     }
